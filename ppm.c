3b7fcee7ee4cd70b06a64a89f9f3468d91662af8
Daniel Church <church.daniel96@gmail.com>
2016-04-25 18:20:27 +0000

    Final comments added to code.

--- ppm.c
+++ ppm.c
@@ -5,19 +5,23 @@ ppmPic *read(char *name){
     ppmPic *ptr = malloc(sizeof(ppmPic));
     FILE *fp = fopen(name, "r");
     
+    
+    //These four commands make the initial reads that provide the array of colors
+    //the number of columns and the number of rows.
     readToken(fp);
     ptr->cols = readInt(fp);
     ptr->rows = readInt(fp);
     ptr->colors = readInt(fp);
-    int i, j;
     
+    //These few commands allocate the matrix of pixels that will hold teh picture data
     int a;
 	Pixel **pix;
 	pix = malloc(sizeof(Pixel *) * ptr->rows);
 	for (a=0; a<ptr->rows; a++)
         pix[a] = malloc(sizeof(Pixel) * ptr->cols);
    
-    
+   //Here we read in all of the pixels colors to the locations where they belong.
+    int i, j;
     for (i = 0; i<ptr->rows; i++){
         for (j=0; j<ptr->cols; j++){
             pix[i][j].red = readInt(fp);
@@ -26,6 +30,7 @@ ppmPic *read(char *name){
         }
     }
     
+    //Assign the pixel matrix to the ppmPic structure.
     ptr->pixels = pix;
     
     fclose(fp);
@@ -37,10 +42,12 @@ void write(char *name, char *name2, ppmPic *head){
     
     ppmPic *ptr = head;
     
+    //Print the number of colors columns and rows.
     fprintf(fp, "P3\n%d %d %d\n", ptr->cols, ptr->rows, ptr->colors);
     Pixel **pix;
     pix = ptr->pixels;
     
+    //Print into the file the values of the pixels at those points
     int i, j;
     for (i = 0; i<ptr->rows; i++){
         for (j=0; j<ptr->cols; j++){
@@ -53,6 +60,8 @@ void write(char *name, char *name2, ppmPic *head){
 }
 
 void copy(char *name, char *name2){
+    //Copy can basically be written by reading in teh first file and writing
+    //that data to the second file.
     ppmPic *head = read(name);
     write(name, name2, head);
     return;
@@ -68,17 +77,20 @@ void grow(char *name, char *name2, ppmPic *head){
     ptr->cols = col*2;
     ptr->rows = row*2;
     
+    //Print the number of colors columns and rows.
     fprintf(fp, "P3\n%d %d %d\n", ptr->cols, ptr->rows, ptr->colors);
     Pixel **pix;
     pix = ptr->pixels;
 
     int i, j;
     for (i = 0; i<row; i++){
+        //This first for loop prints two pixels each itteration on the row.
         for (j=0; j<col; j++){
             fprintf(fp, "%d\n%d\n%d\n", pix[i][j].red, pix[i][j].blue, pix[i][j].green);
             fprintf(fp, "%d\n%d\n%d\n", pix[i][j].red, pix[i][j].blue, pix[i][j].green);
         }
         j = 0;
+        //This for loop prints a second row and two pixels for each itteration.
         for (j=0; j<col; j++){
             fprintf(fp, "%d\n%d\n%d\n", pix[i][j].red, pix[i][j].blue, pix[i][j].green);
             fprintf(fp, "%d\n%d\n%d\n", pix[i][j].red, pix[i][j].blue, pix[i][j].green);
@@ -96,19 +108,25 @@ void shrink(char *name, char *name2, ppmPic *head){
     FILE *fp = fopen(name2, "w");
     ppmPic *ptr = head;
     
+    //If the number of columns or rows is odd we need to cut off the last
+    //one so the average is correct.
     if(col%2 != 0) col = col - 1;
     if(row%2 != 0) row = row - 1;
     
+    //We divide by two to get our final number of columns after the shrink.
     ptr->cols = col/2;
     ptr->rows = row/2;
     
     col = col/2;
     row = row/2;
     
+    //Print the number of colors columns and rows into the file.
     fprintf(fp, "P3\n%d %d %d\n", ptr->cols, ptr->rows, ptr->colors);
     Pixel **pix;
     pix = ptr->pixels;
 
+    //This for loop prints the average pixel value of the four surrounding pixels
+    //into the file.
     int i, j;
     for (i = 0; i<row; i++){
         for (j=0; j<col; j++){
